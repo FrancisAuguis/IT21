28023b7889d49b8c97eeeb48ea8b02a8d04fc743
FrancisAuguis <delacruzfrancis@gmail.com>
2025-11-18 08:39:18 +0800

    added sum and subtractions

---
+++ Math.js
@@ -0,0 +1,62 @@
+function sum(num1,num2){
+sum = num1 + sum2
+console.log(sum)
+
+}
+ sum(2,2);
+ function deff(num1,num2){
+    deff = num1, - num2;
+    console.log (deff);
+
+ }
+
+ deff (2,3);
+
+ function prod(num1,num2){
+         sum = num1 = num2
+         console.log(prod)
+ }
+ prod(2,3);
+
+ function quo(num1,num2){
+    sum = sum1 / sum2
+    console.log(quo);
+
+ }
+ quo(2,3);
+
+ function add(num1,+num2){
+      return num1+num2;
+
+      sum(2,3);
+      deff(2,3);
+      grod(2,3);
+
+ }
+
+ function add(num1,num){
+     return num1+num2;
+
+ }
+
+
+ function deff (num1,num2){
+    return num1,num2;
+
+ }
+ 
+
+
+ function prod(num1,num2){
+       return num1,- num2;
+
+ }
+  
+ function quo(num1,num2){
+      return num1/num2;
+
+ }
+ console.log(add(2,2));
+ console.log(deff(2,2));
+ console.log(prod(2,2));
+ console.log(quo(2,2));
